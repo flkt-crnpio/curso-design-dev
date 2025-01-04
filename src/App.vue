f06078a68610783e5ba1a0de67f453430faae0a8
flkt <flkt.crnpio@gmail.com>
2025-01-04 09:55:59 -0600

    agregando rutas

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <a href="#main">Ir al contenido principal</a>
+  <!-- <a href="#main" class="enlace-main">Ir al contenido principal</a> -->
 
   <nav>
     <RouterLink to="/">Introducción</RouterLink>
-    <RouterLink to="/estudios-de-casos">Estudios de casos</RouterLink>
-    <RouterLink to="/glosario-de-elementos">Glosario de elementos</RouterLink>
+    <RouterLink to="/html-estructura">Estructura</RouterLink>
+    <RouterLink to="/html-contenido">Contenido</RouterLink>
+    <RouterLink to="/css-estilo">Estilo</RouterLink>
+    <RouterLink to="/html-css-integracion">Integración</RouterLink>
   </nav>
 
   <RouterView />
 
-  <footer>
-    pie de página
-  </footer>
 </template>
